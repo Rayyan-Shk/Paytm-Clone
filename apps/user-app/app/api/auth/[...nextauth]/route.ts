88a1d814d5b448288c77f3f178b441f2f0c1792f
Rayyan-Shk <rayyanshaikhassociates@gmail.com>
2024-08-26 21:55:53 +0530

    completed and fixed next auth

--- apps/user-app/app/api/auth/[...nextauth]/route.ts
+++ apps/user-app/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import { authOptions } from "../../../lib/auth"
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth({
+    ...authOptions,
+pages: {
+    signIn:"/auth/signIn"
+}})
 
 export { handler as GET, handler as POST }
