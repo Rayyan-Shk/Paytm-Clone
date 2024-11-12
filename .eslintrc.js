0bb502f5c9722c8b2697ac1da9e6136ab85fa1e2
Rayyan-Shk <rayyanshaikhassociates@gmail.com>
2024-11-12 16:52:42 +0530

    updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
   extends: ["@repo/eslint-config/library.js"],
+  "rules": {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
